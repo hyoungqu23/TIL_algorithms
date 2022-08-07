346350c636572e0289fdc7410b133edd4fb1daaf
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-07 23:17:55 +0900

    [Sorting] bubble Sorting

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,49 @@
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+
+const bubbleSort = (array) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      console.log(i, j);
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+      console.log(array);
+    }
+  }
+  return array;
+};
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+bubbleSort([4, 3, 2154, 612, 33]);
+bubbleSort([99, 123, 5, 22, 13]);
